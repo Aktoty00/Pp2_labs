2f0c200be15fed53c06b5f47d41d353bf751f47b
Aktoty00 <aktoty.rysdaulet@mail.ru>
2018-01-14 14:29:39 +0600

    first commit

---
+++ ConsoleApplication1/ConsoleApplication2/ConsoleApplication2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Class1
+    {
+        public float redius;
+
+    }
+}
