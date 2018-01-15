141a6845e3dca2e89eb09d9afe669c1b6b5b0db3
Aktoty00 <aktoty.rysdaulet@mail.ru>
2018-01-15 23:28:56 +0600

    first commit

---
+++ circlee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    class circle
+    {
+        public double radius;
+        public circle()
+        {
+            radius = 55;
+        }
+        public circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override string ToString()
+        {
+            return "radius=" + radius;
+        }
+        public static double diameter(double radius)
+        {
+            return 2 * radius;
+        }
+        public static double area(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        public static double perimeter(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                circle c = new circle();
+                double r = double.Parse(Console.ReadLine());
+                c.radius = r;
+                Console.WriteLine(c);
+                Console.WriteLine(circle.diameter(r));
+                Console.WriteLine(circle.area(r));
+                Console.WriteLine(circle.perimeter(r));
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
