bb8139f46ab493a66ec18ad1accdb331358c0860
Aktoty00 <aktoty.rysdaulet@mail.ru>
2018-02-06 12:08:41 +0600

    complex

---
+++ lab2/complex_division/complex_division/complex_division.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complex_division
+{
+    class complex_division
+    {
+        public int a, b;
+        public complex_division(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+        public static complex_division operator /(complex_division c1, complex_division c2)
+        {
+            int a_ = c1.a * c2.b;
+            int b_ = c2.a * c1.b;
+            complex_division c = new complex_division(a_, b_);
+            c.Simplify();
+            return c;
+        }
+        public static int gcd(int x, int y)
+        {
+            if (y == 0)
+            {
+                return x;
+            }
+            return gcd(y, x % y);
+        }
+        public void Simplify()
+        {
+            int z = gcd(a, b);
+            a /= z;
+            b /= z;
+        }
+    }
+}
