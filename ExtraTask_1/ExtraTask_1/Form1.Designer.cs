01240da62e2d2a01ea96b09b67efc7e6463e667a
Aktoty00 <aktoty.rysdaulet@mail.ru>
2018-03-14 20:35:02 +0600

    Task for bonus points

---
+++ ExtraTask_1/ExtraTask_1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ExtraTask_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(108, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ ExtraTask_1/ExtraTask_1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtraTask_1
+{
+    public partial class Form1 : Form
+    {
+        int score= 0;
+        List<Button> buttonlist = new List<Button>();
+        public Form1()
+        {
+            InitializeComponent();
+            Height = 600;
+            Width = 600;
+            timer1.Start();
+            timer2.Start();
+            button1.Location = new Point(260, 539);
+
+        }
+        private void NewButtons()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(1, 595);
+            Button btn = new Button();
+            buttonlist.Add(btn);
+            btn.Size = new Size(25, 25);
+            btn.Location = new Point(x, 0);
+            btn.Text = " ";
+            btn.BackColor = Color.Coral;
+            Controls.Add(btn);
+         
+        }
+        int speed = 5;
+
+        private void Moves()
+        {
+            foreach (Button b in buttonlist)
+            {
+
+                b.Location = new Point(b.Location.X, b.Location.Y + speed);
+                if (b.Location.Y == 545)
+                {
+                    score++;
+                }
+                if (Math.Abs(b.Location.Y - button1.Location.Y) <= button1.Size.Height && Math.Abs(b.Location.X - button1.Location.X) <= button1.Size.Width)
+                {
+                    timer1.Stop();
+                    timer2.Stop();
+
+                    MessageBox.Show("Game Over!!!\n" + "Score is: " + score);
+                    score = 0;
+                    button1.Text = (score).ToString();                   
+                }
+
+            }
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NewButtons();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button1.Text = (score).ToString();
+            Moves();
+         
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case (char)49:
+                    button1.Location = new Point(button1.Location.X - 5, button1.Location.Y);
+                    break;
+                case (char)51:
+                    button1.Location = new Point(button1.Location.X + 5, button1.Location.Y);
+                    break;
+            }
+        }
+    }
+}
