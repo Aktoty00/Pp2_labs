29b2e46e05bb20537f645d626d8766581b1fad93
Aktoty00 <aktoty.rysdaulet@mail.ru>
2018-04-11 20:43:47 +0600

    endterm

---
+++ endterrm3/endterrm3/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace endterrm3
+{
+    class Car
+    {
+        public GraphicsPath gp;
+        public int direction = 1;
+        public int x, y;
+        public Car(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+       
+        public void Draw(Graphics g)
+        {
+            gp = new GraphicsPath();
+            int w = 60;
+            int h = 10;
+            gp.AddRectangle(new Rectangle(x, y, w, h));
+
+            Point[] p = { new Point (x +5 , y),  new Point(x+w - 5, y), new Point(x +w-9, y-10),new Point(x + 9, y - 10) };
+            gp.AddPolygon(p);
+            gp.AddEllipse(new Rectangle( x + 8,y+h-2, 5, 5));
+            gp.AddEllipse(new Rectangle(x +w-8,y + h - 2 ,5, 5));
+
+            //direction = 1 ----right
+
+            g.DrawPath(new Pen(Color.Red, 2), gp);
+        }
+    
+    }
+}
---
+++ endterrm3/endterrm3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace endterrm3
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+            timer1.Start();
+        }
+        Car car = new Car(50,50);
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //direction = 1 ----right
+           
+            if (car.direction == 1)
+                car.x += 5;
+           
+            g.Clear(Color.White);
+            car.Draw(g);
+        }
+    }
+}
