0b8f47790b5201bac8e4ec6346eedff6cf502330
Aktoty00 <aktoty.rysdaulet@mail.ru>
2018-04-17 23:06:15 +0600

    sea__battle

---
+++ sea__battle/sea__battle/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace sea__battle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.small_button = new System.Windows.Forms.Button();
+            this.midle_button = new System.Windows.Forms.Button();
+            this.big_button = new System.Windows.Forms.Button();
+            this.biggest_button = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // small_button
+            // 
+            this.small_button.Location = new System.Drawing.Point(234, 408);
+            this.small_button.Name = "small_button";
+            this.small_button.Size = new System.Drawing.Size(68, 29);
+            this.small_button.TabIndex = 6;
+            this.small_button.Text = "Small(1)";
+            this.small_button.UseVisualStyleBackColor = true;
+            this.small_button.Click += new System.EventHandler(this.Boat_Click);
+            // 
+            // midle_button
+            // 
+            this.midle_button.Location = new System.Drawing.Point(160, 408);
+            this.midle_button.Name = "midle_button";
+            this.midle_button.Size = new System.Drawing.Size(68, 29);
+            this.midle_button.TabIndex = 5;
+            this.midle_button.Text = "Midle(2)";
+            this.midle_button.UseVisualStyleBackColor = true;
+            this.midle_button.Click += new System.EventHandler(this.Boat_Click);
+            // 
+            // big_button
+            // 
+            this.big_button.Location = new System.Drawing.Point(86, 408);
+            this.big_button.Name = "big_button";
+            this.big_button.Size = new System.Drawing.Size(68, 29);
+            this.big_button.TabIndex = 4;
+            this.big_button.Text = "Big(3)";
+            this.big_button.UseVisualStyleBackColor = true;
+            this.big_button.Click += new System.EventHandler(this.Boat_Click);
+            // 
+            // biggest_button
+            // 
+            this.biggest_button.Location = new System.Drawing.Point(12, 408);
+            this.biggest_button.Name = "biggest_button";
+            this.biggest_button.Size = new System.Drawing.Size(68, 29);
+            this.biggest_button.TabIndex = 3;
+            this.biggest_button.Text = "Biggest(4)";
+            this.biggest_button.UseVisualStyleBackColor = true;
+            this.biggest_button.Click += new System.EventHandler(this.Boat_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(399, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(351, 351);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 351);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(603, 408);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 29);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Vertical";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.vertical_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(664, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Horizontal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.horizontal_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(481, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Horizontal";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LawnGreen;
+            this.button7.Location = new System.Drawing.Point(348, 408);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 29);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Start";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Start_Clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 461);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.small_button);
+            this.Controls.Add(this.midle_button);
+            this.Controls.Add(this.big_button);
+            this.Controls.Add(this.biggest_button);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button biggest_button;
+        private System.Windows.Forms.Button big_button;
+        private System.Windows.Forms.Button midle_button;
+        private System.Windows.Forms.Button small_button;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ sea__battle/sea__battle/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sea__battle
+{
+    public partial class Form1 : Form
+    {
+        Pen pen = new Pen(Color.Black, 2);
+        bool boat_clicked;
+        bool start_clicked;
+        Bitmap bmp1, bmp2;
+        Graphics g1, g2;
+        Point current, picbox2;
+        bool horizontal, vertical;
+        public enum Tool
+        {
+            biggest, big, midle ,small
+        };
+        public enum State
+        {
+            Start, Finish, Missed
+        };
+        Tool tool;
+        State state;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp1 = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            g1 = Graphics.FromImage(bmp1);
+            pictureBox1.Image = bmp1;
+            bmp2 = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            g2 = Graphics.FromImage(bmp2);
+            pictureBox2.Image = bmp2;
+            Draw();
+            tool = Tool.biggest;
+            horizontal = true;
+            vertical = false;
+            start_clicked = false;
+        }
+        public void Draw()
+        {
+            for (int i = 0; i < 11; i++)
+                g1.DrawLine(pen, 35 * i, 0, 35 * i, 350);
+            for (int i = 0; i < 11; i++)
+                g1.DrawLine(pen, 0, 35 * i, 350, 35 * i);
+            for (int i = 0; i < 11; i++)
+                g2.DrawLine(pen, 35 * i, 0, 35 * i, 350);
+            for (int i = 0; i < 11; i++)
+                g2.DrawLine(pen, 0, 35 * i, 350, 35 * i);
+        }
+        private void Boat_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if(btn.Text == "Biggest(4)")
+                tool = Tool.biggest;
+            if (btn.Text == "Big(3)")
+                tool = Tool.big;
+            if (btn.Text == "Midle(2)")
+                tool = Tool.midle;
+            if (btn.Text == "Small(1)")
+                tool = Tool.small;
+        }
+        private void horizontal_Click(object sender, EventArgs e)
+        {
+            horizontal = true;
+            vertical = false;
+            label1.Text = "Horizontal";
+        }
+
+        private void vertical_Click(object sender, EventArgs e)
+        {
+            horizontal = false;
+            vertical = true;
+            label1.Text = "Vertical";
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            boat_clicked = true;
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            picbox2 = e.Location;
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            int x, y;
+            x = picbox2.X / 35 *35;
+            y = picbox2.Y / 35 * 35;
+            if (start_clicked)
+            {
+                e.Graphics.DrawLine(new Pen(Color.Red, 2), x, y, x + 35, y + 35);
+                e.Graphics.DrawLine(new Pen(Color.Red, 2), x, y + 35, x + 35, y);
+                pictureBox2.Refresh();
+            }
+        }
+
+        private void Start_Clicked(object sender, EventArgs e)
+        {
+            start_clicked = true;
+            state = State.Start;
+            biggest_button.Enabled = false;
+            big_button.Enabled = false;
+            midle_button.Enabled = false;
+            small_button.Enabled = false;
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            int x, y;
+            x = picbox2.X / 35 * 35;
+            y = picbox2.Y / 35 *35;
+            if (start_clicked)
+            {
+                g2.DrawLine(new Pen(Color.Red, 2), x, y, x + 35, y + 35);
+                g2.DrawLine(new Pen(Color.Red, 2), x, y + 35, x + 35, y);
+                pictureBox2.Refresh();
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            current = e.Location;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int x, y;
+            x = current.X / 35;
+            x *= 35;
+            y = current.Y / 35;
+            y *= 35;
+            if (boat_clicked && horizontal)
+            {
+                if (tool == Tool.biggest)
+                {
+                    if (current.X > 210)
+                        x = 210;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 140, 35);
+                }
+                if (tool == Tool.big)
+                {
+                    if (current.X > 245)
+                        x = 245;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 105, 35);
+                }
+                if (tool == Tool.midle)
+                {
+                    if (current.X > 280)
+                        x = 280;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 70, 35);
+                }
+                if (tool == Tool.small)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 35);
+                }
+            }
+            if (boat_clicked && vertical)
+            {
+                if (tool == Tool.biggest)
+                {
+                    if (current.Y > 210)
+                        y = 210;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 140);
+                }
+                if (tool == Tool.big)
+                {
+                    if (current.Y > 245)
+                        y = 245;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 105);
+                }
+                if (tool == Tool.midle)
+                {
+                    if (current.Y > 280)
+                        y = 280;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 70);
+                }
+                if (tool == Tool.small)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 35);
+                }
+            }
+            pictureBox1.Refresh();
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int x, y;
+            x = current.X / 35;
+            x *= 35;
+            y = current.Y / 35;
+            y *= 35;
+            if (boat_clicked && horizontal)
+            {
+                if (tool == Tool.biggest)
+                {
+                    if (current.X > 210)//6*35
+                        x = 210;
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 140, 35);
+                }
+                if (tool == Tool.big)
+                {
+                    if (current.X > 245)//7*35
+                        x = 245;
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 105, 35);
+                }
+                if (tool == Tool.midle)
+                {
+                    if (current.X > 280)//8*35
+                        x = 280;
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 70, 35);
+                }
+                if (tool == Tool.small)
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 35);
+            }
+            if (boat_clicked && vertical)
+            {
+                if (tool == Tool.biggest)
+                {
+                    if (current.Y > 210)//6*35
+                        y = 210;
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 140);
+                }
+                if (tool == Tool.big)
+                {
+                    if (current.Y > 245)//7*35
+                        y = 245;
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 105);
+                }
+                if (tool == Tool.midle)
+                {
+                    if (current.Y > 280)//8*35
+                        y = 280;
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 70);
+                }
+                if (tool == Tool.small)
+                    g1.FillRectangle(new SolidBrush(Color.Black), x, y, 35, 35);
+            }
+            boat_clicked = false;
+        }
+    }
+}
